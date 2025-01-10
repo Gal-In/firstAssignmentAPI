710d565cbf4bb597a725e831dffd3e75980fe2f1
unknown <galinbar2411@gmail.com>
2025-01-10 11:28:32 +0200

    add basic swagger config

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import usersRoutes from "./routes/userRoutes";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 
 const app = express();
 dotenv.config();
@@ -20,6 +22,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/posts", postRoutes);
 app.use("/comments", commentsRoutes);
 app.use("/users", usersRoutes);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Assignment",
+      version: "1.0.0",
+      description: "rest server description",
+    },
+    servers: [{ url: "http://localhost:3000" }],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`);
