85d9ee6b04b3ebbcc2a8b9e5d95703ef810af560
unknown <galinbar2411@gmail.com>
2025-01-11 12:16:23 +0200

    add auth swagger

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -9,6 +9,42 @@ const router = express.Router();
  *   description: Users API
  */
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - username
+ *         - email
+ *         - password
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: Db generated user id
+ *         username:
+ *           type: string
+ *           description: The user username
+ *         email:
+ *           type: string
+ *           description: The user email
+ *         password:
+ *           type: string
+ *           description: The user password, saved after encryption
+ *         refreshTokens:
+ *           type: array
+ *           items:
+ *              type: string
+ *           description: The user refresh tokens, created using the authentication requests
+ *       example:
+ *         _id: 67813c84b8f996b54a5e04ca
+ *         username: koren
+ *         email: koren@gmail.com
+ *         password: $2b$10$OgtMmMCzD3s29LUk6HeyP.Wksi0eAGRYKDvNgBklU476hPDqf45m.
+ *         refreshTokens: []
+ */
+
 router.post("/", users.addNewUser);
 router.get("/", users.getAllUsers);
 router.put("/:id", users.updateUserEmail);
